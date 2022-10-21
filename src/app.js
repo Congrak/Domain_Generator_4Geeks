a89bf1ee57fb27860ba1023fa30c07b0dbfeffa5
Congrak <rodricelis@gmail.com>
2022-10-21 18:39:53 +0000

    Domain Generetor by Rodrigo Celis

--- src/app.js
+++ src/app.js
@@ -8,4 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dot = [".com", ".net"];
+  let coun = [".us", ".es"];
+
+  let domain = [];
+
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let d = 0; d < dot.length; d++) {
+          for (let c = 0; c < coun.length; c++) {
+            domain.push(pronoun[p] + adj[a] + noun[n] + dot[d] + coun[c]);
+          }
+        }
+      }
+    }
+  }
+  document.querySelector("#domain").innerHTML = domain.join("\r");
 };
